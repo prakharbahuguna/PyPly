c6f0aae47942c84d882ba324752229ce3b813562
Matt Akerman <m.ak@me.com>
2015-04-17 22:41:15 +0100

    Removed serverside components - moving to separate repo as there are separate requirements/dependencies

--- tests/client_unittests.py
+++ tests/client_unittests.py
@@ -3,8 +3,9 @@ __author__ = 'matt'
 import unittest
 
 class MyTestCase(unittest.TestCase):
+
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
 if __name__ == '__main__':
     unittest.main()
