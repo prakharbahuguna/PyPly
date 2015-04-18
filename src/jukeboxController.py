68f92a724adf701f8a57643b2f58cb7f1c3155ac
Prakhar Bahuguna <me@prakhar.uk>
2015-04-18 15:36:05 +0100

    JukeboxController now receives commands from message broker. Removed debug code.

--- src/jukeboxController.py
+++ src/jukeboxController.py
@@ -2,11 +2,12 @@ __author__ = 'matt'
 
 import threading
 import spotify_wrapper
+from zeroMqBroker import ZeroMQBroker
 from time import sleep
 from collections import deque
 
-class JukeboxController:
 
+class JukeboxController:
     def __init__(self):
         self.playlist = None
         self.running = False
@@ -16,16 +17,13 @@ class JukeboxController:
 
     def spotifyController(self):
         while self.running:
-            status = spotify_wrapper.player_state()
-            print status
-            
-            if not self.playlist:
-                print "Empty playlist - get some tunes sent in!"
-            elif status == 'paused' or status == 'stopped':
-                nextTrack = self.playlist.popleft()
-                self.playlist.append(nextTrack)
-                spotify_wrapper.play_track(nextTrack)
-                print "Now playing " + nextTrack
+            if self.playlist:
+                status = spotify_wrapper.player_state()
+                if status == 'paused' or status == 'stopped':
+                    nexttrack = self.playlist.popleft()
+                    self.playlist.append(nexttrack)
+                    spotify_wrapper.play_track(nexttrack)
+                    print "Now playing " + nexttrack
             sleep(1)
 
         spotify_wrapper.play_pause()
@@ -34,6 +32,11 @@ class JukeboxController:
 if __name__ == "__main__":
     jukebox = JukeboxController()
     jukebox.running = True
-    t = threading.Thread(target=jukebox.spotifyController)
-    t.start()
-    t.join()
+    jukeThread = threading.Thread(name='SpotifyController', target=jukebox.spotifyController)
+
+    mqBroker = ZeroMQBroker(jukebox)
+    brokerThread = threading.Thread(name='MessageListener', target=mqBroker.messageListener, args=('house1',))
+
+    jukeThread.start()
+    brokerThread.start()
+    jukeThread.join()
--- src/zeroMqBroker.py
+++ src/zeroMqBroker.py
@@ -3,33 +3,32 @@ __author__ = 'matt'
 import zmq
 import json
 
+
 class ZeroMQBroker:
     LISTEN_PORT = "5556"
 
-    def __init__(self):
+    def __init__(self, jukebox):
         # Socket to talk to server
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.SUB)
-        self.socket.connect("tcp://localhost:%s" % self.LISTEN_PORT)
+        self.socket.connect('tcp://localhost:{}'.format(self.LISTEN_PORT))
+        self.jukebox = jukebox
 
-    def startMessageListener(self, partyID):
+    def messageListener(self, partyID):
         self.socket.setsockopt(zmq.SUBSCRIBE, partyID)
 
         while True:
             message = self.socket.recv_string()
-            messageParts = message.split()
-            topic = messageParts[0]
-            verb = messageParts[1]
+            message_parts = message.split()
+            topic = message_parts[0]
+            verb = message_parts[1]
             argument = None
 
-            if len(messageParts) > 2:
-                argument = messageParts[2]
+            if len(message_parts) > 2:
+                argument = message_parts[2]
 
-            if verb == "loadPlaylist":
-                spotifyURIs = json.loads(messageParts[2])
-
-
-
-if __name__ == "__main__":
-    test = ZeroMQBroker()
-    test.startMessageListener("house1")
+            if verb == 'loadPlaylist':
+                self.jukebox.setPlaylist(json.loads(message_parts[2]))
+            elif verb == 'quit':
+                self.jukebox.running = False
+                return
